bef3f19a5b5a7e610f9d4ae0715cda431d440cd6
iamarvy <okeseyi5@gmail.com>
2025-10-08 18:52:44 +0100

    Refactor code structure for improved readability and maintainability

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: [
-    "@nuxt/content",
     "@nuxt/icon",
     "shadcn-nuxt",
     "nuxt-auth-utils",
