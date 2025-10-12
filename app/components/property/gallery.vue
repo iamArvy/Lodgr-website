6e7df799112cac5e049a974dfc439c9001dbf755
iamarvy <okeseyi5@gmail.com>
2025-10-12 19:06:41 +0100

    refactor: update property gallery implementation and enhance UI details

--- app/components/property/gallery.vue
+++ app/components/property/gallery.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 import type { PropertyGalleryProps } from '~/interfaces';
 defineProps<PropertyGalleryProps>()
-import Fade from 'embla-carousel-fade';
-const plugins = [
-  Fade()
-]
+const { settings, emblaMainApi, selectedIndex } = usePropertyGallery();
 </script>
 <template>
-  <Carousel :opts="{
-    align: 'start',
-    loop: false,
-  }" :plugins="plugins" class="relative group">
+  <Carousel v-bind="settings" @init-api="(val) => emblaMainApi = val" class="relative group">
     <div
       class="absolute z-5 w-full top-1/2 -translate-y-1/2 hidden md:group-hover:flex items-center justify-between px-2 transition-all ease-in-out duration-500">
       <CarouselPrevious />
@@ -18,8 +12,12 @@ const plugins = [
     </div>
     <CarouselContent>
       <CarouselItem v-for="image in gallery" :key="image">
-        <figure class="overflow-hidden rounded-2xl md:aspect-[2/1] lg:aspect-[5/2]">
+        <figure class="relative overflow-hidden rounded-2xl md:aspect-[2/1] lg:aspect-video">
           <img :src="image" alt="" class="w-full object-cover">
+          <div class="flex space-x-2 w-full justify-center mt-2 absolute bottom-2">
+            <span v-for="(_, index) in gallery" :key="index" class="p-1 w-2 h-2 rounded-full"
+              :class="index === selectedIndex ? 'bg-primary w-5' : 'bg-white'" />
+          </div>
         </figure>
       </CarouselItem>
     </CarouselContent>
--- app/pages/homes/[id].vue
+++ app/pages/homes/[id].vue
@@ -16,43 +16,77 @@ const infoItems = [
     value: baths
   }
 ]
+
+const headDetails = [
+  {
+    icon: "lucide:map-pin",
+    value: location
+  },
+  {
+    icon: "material-symbols:star",
+    value: `${rating} (${reviewsCount} reviews)`
+  },
+  {
+    icon: "material-symbols:bed",
+    value: `${rooms} Bedrooms`
+  },
+  {
+    icon: "material-symbols:bathtub",
+    value: `${baths} Bathrooms`
+  },
+]
 useAppTitle(name);
+const inFavourites = ref(false);
+
 </script>
 <template>
   <main class="space-y-5 container mx-auto">
-    <section class="relative">
-      <PropertyGallery :gallery="property.gallery" />
-      <Button class="absolute top-5 right-5">
-        <Icon name="lucide:heart" />
-        <span class="hidden md:inline-block">Save</span>
-      </Button>
-    </section>
-    <section class="space-y-1 sm:flex">
-      <div class="flex-1 space-y-1">
-        <h1 class="text-xl md:text-3xl lg:text-5xl font-bold">{{ name }}</h1>
-        <p class="flex items-center gap-1">
-          <MapPin class="w-5 h-5 text-primary" /> {{ location }}
+    <section class="lg:grid grid-cols-3 gap-3 space-y-4">
+      <div class="relative ">
+        <PropertyGallery :gallery="property.gallery" />
+        <Button class="absolute top-2 right-2 flex items-center gap-1" variant="outline" size="icon"
+          @click="inFavourites = !inFavourites">
+          <Icon :name="inFavourites ? 'material-symbols:favorite-rounded' : 'material-symbols:favorite-outline-rounded'"
+            size=" 20" class="text-primary" />
+        </Button>
+      </div>
+      <div class="flex-1 space-y-1 col-span-2">
+        <h1 class="text-2xl md:text-3xl lg:text-5xl font-bold">{{ name }}</h1>
+        <p v-for="{ icon, value } in headDetails" class="flex items-center gap-1">
+          <Icon :name="icon" class="inline-block text-primary" size="20" /> {{ value }}
         </p>
-        <p class="flex items-center gap-0.5">
-          <span class="flex items-center">
-            <Icon v-for="n in Math.floor(rating)" :key="n" name="material-symbols:star"
-              class="inline-block text-secondary" size="18" />
-          </span>
-          <span class="font-bold">{{ rating }}</span>
-          <span> ({{ reviewsCount }} reviews)</span>
-        </p>
-        <div class="flex gap-5">
-          <div v-for="{ value, icon, label } in infoItems" class="flex items-center gap-2">
-            <Icon :name="icon" size="22" class="text-primary" />
-            <span class="font-semibold">{{ value }} {{ label }}</span>
-          </div>
+        <div class="flex gap-1">
+          <BookingForm />
+          <ScheduleForm />
         </div>
       </div>
-      <div class="flex gap-1">
-        <BookingForm />
-        <ScheduleForm />
-      </div>
     </section>
+    <!-- <section class=" space-y-1 sm:flex">
+          <div class="flex-1 space-y-1">
+            <h1 class="text-xl md:text-3xl lg:text-5xl font-bold">{{ name }}</h1>
+            <p class="flex items-center gap-1">
+              <MapPin class="w-5 h-5 text-primary" /> {{ location }}
+            </p>
+            <p class="flex items-center gap-0.5">
+              <span class="flex items-center">
+                <Icon v-for="n in Math.floor(rating)" :key="n" name="material-symbols:star"
+                  class="inline-block text-secondary" size="18" />
+              </span>
+              <span class="font-bold">{{ rating }}</span>
+              <span> ({{ reviewsCount }} reviews)</span>
+            </p>
+            <div class="flex gap-5">
+              <div v-for="{ value, icon, label } in infoItems" class="flex items-center gap-2">
+                <Icon :name="icon" size="22" class="text-primary" />
+                <span class="font-semibold">{{ value }} {{ label }}</span>
+              </div>
+            </div>
+          </div>
+          <div class="flex gap-1">
+            <BookingForm />
+            <ScheduleForm />
+          </div>
+    </section> -->
     <section>
       <PropertyInformation v-bind="property" />
     </section>
